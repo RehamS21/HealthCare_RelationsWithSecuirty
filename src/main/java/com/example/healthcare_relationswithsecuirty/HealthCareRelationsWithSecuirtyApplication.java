28589000a995b84df883f480a020740823c4fd68
RehamS21 <reham.mu433@gmail.com>
2023-08-29 11:24:35 +0300

    Initial commit

---
+++ src/main/java/com/example/healthcare_relationswithsecuirty/HealthCareRelationsWithSecuirtyApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthcare_relationswithsecuirty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareRelationsWithSecuirtyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareRelationsWithSecuirtyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/healthcare_relationswithsecuirty/HealthCareRelationsWithSecuirtyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthcare_relationswithsecuirty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareRelationsWithSecuirtyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
